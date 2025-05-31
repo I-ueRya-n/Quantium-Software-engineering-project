4c3eee77037a8d074761507c4df037c640b65937
root <root@IUE.localdomain>
2025-05-31 12:03:31 +1000

    Task2 completed

---
+++ Visualisation.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from dash import Dash, html, dcc
+import plotly.express as px
+
+
+
+DATA_PATH = './formatted_data.csv'
+
+data = pd.read_csv(DATA_PATH)
+data = data.sort_values(by = 'date')
+
+app = Dash()
+
+lineChart = px.scatter(data, x = 'date', y = 'sales', title = 'Pink morsel Sales')
+visualisation = dcc.Graph(
+    id = 'Visualistion',
+    figure = lineChart
+)
+
+header = html.H1(
+    'PINK MORSEL SALES',
+    id = 'header'
+)
+
+
+app.layout = html.Div(
+    [
+        header,
+        visualisation
+    ]
+)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
